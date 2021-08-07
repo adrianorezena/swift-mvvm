483b1618d9c9f5ebe51a4d4ac6d6ab50d171a94d
adrianorezena <adrianorezena@users.noreply.github.com>
2021-08-07 16:07:51 -0300

    project started

---
+++ swift_mvvm/swift_mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_mvvm
+//
+//  Created by Adriano Rezena on 07/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
